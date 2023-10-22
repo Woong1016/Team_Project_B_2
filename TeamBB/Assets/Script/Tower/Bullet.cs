d0934edcd4c18bafb0b9b57b6dd3ee5b19b2b61b
jintg0821 <jintg0825@gmail.com>
2023-10-23 01:50:05 +0900

    feat:"상점 기능 구현"

--- TeamBB/Assets/Script/Tower/Bullet.cs
+++ TeamBB/Assets/Script/Tower/Bullet.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     public Vector3 launchDirection; // 발사 방향 벡터 선언
-    public float bulletSpeed = 10f; // 탄환 속도
+    public float bulletSpeed = 10000f; // 탄환 속도
+    public float damage = 10f;      // 탄환의 공격력
 
     private void Start()
     {
@@ -17,8 +18,7 @@ public class Bullet : MonoBehaviour
     {
         if (other.CompareTag("Wall") || other.CompareTag("Floor"))
         {
-            Debug.Log("충돌: " + other.gameObject.name);
-
+            //Debug.Log("충돌: " + other.gameObject.name);
             Destroy(gameObject);
         }
         else if (other.CompareTag("SpawnedObject"))
@@ -26,9 +26,9 @@ public class Bullet : MonoBehaviour
             Enemy enemy = other.GetComponent<Enemy>();
             if (enemy != null)
             {
-                enemy.TakeDamage(10f);
+                // 공격력에 따라 데미지 입히기
+                enemy.TakeDamage(damage);
             }
-
             Destroy(gameObject);
         }
     }
