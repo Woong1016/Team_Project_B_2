5a61a332fc3763e4dd4e5669b323577f0f5b35c0
Woong1016 <yol7122@naver.com>
2023-11-28 22:34:26 +0900

    민트초코 프리팹 수정

--- TeamBB/Assets/Script/Tower/SlowDownObjects.cs
+++ TeamBB/Assets/Script/Tower/SlowDownObjects.cs
@@ -7,24 +7,16 @@ public class SlowDownObjects : MonoBehaviour
 
     void OnTriggerEnter(Collider other)
     {
-        // 충돌한 오브젝트가 태그를 가지고 있는지 확인합니다.
-        CheckAndSlowDown(other);
+        // Do nothing for OnTriggerEnter
     }
 
     void OnCollisionEnter(Collision collision)
     {
-        // 충돌한 오브젝트가 태그를 가지고 있는지 확인합니다.
-        CheckAndSlowDown(collision.collider);
-    }
-
-    // 태그를 가지고 있다면 속도를 느리게 하는 메서드
-    void CheckAndSlowDown(Collider collider)
-    {
         // 충돌한 오브젝트가 지정한 태그를 가지고 있는지 확인합니다.
-        if (collider.CompareTag(targetTag))
+        if (collision.collider.CompareTag(targetTag))
         {
             // 충돌한 오브젝트의 Rigidbody 컴포넌트를 가져옵니다.
-            Rigidbody rb = collider.GetComponent<Rigidbody>();
+            Rigidbody rb = collision.collider.GetComponent<Rigidbody>();
 
             // Rigidbody가 존재하고 속도를 느리게 합니다.
             if (rb != null)
